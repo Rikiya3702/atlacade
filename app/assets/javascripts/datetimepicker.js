e92f285a913013df38065945beab1bfcbcd6de92
Rikiya3702 <rikiya3702@gmail.com>
2019-11-29 01:43:52 +0900

    Add i18n(日本語化)とリファクタリング

--- app/assets/javascripts/datetimepicker.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-    $('#datetimepicker4').datetimepicker({
-        format: 'L'
-    });
-});
---
+++ app/assets/javascripts/form_image.js
@@ -0,0 +1,24 @@
+//参考URL
+// https://whatsupguys.net/programming-school-dive-into-code-learning-88/
+$(function(){
+  $fileField = $('#file')
+  // 選択された画像を取得し表示
+  $($fileField).on('change', $fileField, function(e) {
+    file = e.target.files[0]
+    reader = new FileReader(),
+    $preview = $("#img_field");
+
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+          src: e.target.result,
+          width: "100%",
+          class: "preview",
+          title: file.name
+        }));
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+});
